a97efcc20b34375d57d5350fc191ca2fb292106b
JJport <35145244+JJport@users.noreply.github.com>
2018-01-06 01:40:07 +0900

    Add files via upload

---
+++ mining.sh
@@ -0,0 +1,7 @@
+echo "#####################################"
+echo "# !BitZeny!   zny.coiner.site       #"
+echo "#####################################"
+
+./minerd -a yescrypt -o stratum+tcp://zny.coiner.site:19666 -u JJport.JJport -p 2525Junjun
+
+pause
